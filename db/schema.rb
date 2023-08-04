41a84b23f58c4ffb09514d36a46d759769a530f0
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-03 17:29:15 -0700

    Add Comment model linked to Post and User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_235936) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_001629) do
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "message"
@@ -26,5 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_03_235936) do
     t.string "name"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
